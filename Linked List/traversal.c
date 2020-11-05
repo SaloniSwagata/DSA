6972eaaa737437f0c806c20b2e4be2c1db11a389
Saloni <saloniswagata@gmail.com>
2020-11-05 19:27:33 +0530

    Linked List Creation & Traversal

---
+++ Linked List/traversal.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node * next;
+};
+void LinkedListTraversal(struct Node *ptr)
+{
+    while(ptr != NULL)
+    {
+        printf("Element is: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+int main()
+{
+    struct Node * head;
+    struct Node * second;
+    struct Node * third;
+
+    //Allocate memory for Linked List in heap
+    head = (struct Node *) malloc(sizeof(struct Node));
+    second = (struct Node *) malloc(sizeof(struct Node));
+    third = (struct Node *) malloc(sizeof(struct Node));
+    
+    //Link first and second nodes
+    head->data = 7;
+    head->next = second;
+
+    //Link second and third nodes
+    second->data = 11;
+    second->next = third;
+
+    //Link the third node to null
+    third->data = 13;
+    third->next = NULL;
+
+    LinkedListTraversal(head);
+    return 0;
+}
