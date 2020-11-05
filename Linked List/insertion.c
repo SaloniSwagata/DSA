d0578c9e2425cbb5745f5e2c6c41369f344e2b60
Saloni <saloniswagata@gmail.com>
2020-11-05 20:22:17 +0530

    Insertion in between of the linked list

--- Linked List/insertion.c
+++ Linked List/insertion.c
@@ -20,6 +20,22 @@ struct Node * InsertAtFront(struct Node *head, int data)
     ptr->next = head;
     return ptr;
 }
+
+struct Node * InsertInBetween(struct Node *head, int data, int index)
+{
+    struct Node *ptr = (struct Node*)malloc (sizeof(struct Node));
+    struct Node *p = head;
+    int i=0;
+    while(i!=index-1)
+    {
+        p = p->next;
+        i++;
+    }
+    ptr->data = data;
+    ptr->next = p->next;
+    p->next = ptr;
+    return head;
+}
 int main()
 {
     struct Node * head;
@@ -45,8 +61,13 @@ int main()
 
     printf("Original Linked List: \n");
     LinkedListTraversal(head);
+
     head = InsertAtFront(head, 56);
     printf("Linked List after insertion at front: \n");
+    LinkedListTraversal(head);
+
+    head = InsertInBetween(head, 25, 1);
+    printf("Linked List after insertion in between: \n");
     LinkedListTraversal(head);
     return 0;
 }
