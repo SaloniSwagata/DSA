e39cc15d6bcb8521536e3c753ef6bff704f1bcd5
Saloni <saloniswagata@gmail.com>
2020-12-21 19:59:52 +0530

    Largest sum contiguous subarray

---
+++ Arrays/q8.cpp
@@ -0,0 +1,30 @@
+//find Largest sum contiguous Subarray
+
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+
+    //User Input
+    for(int i=0;i<n;i++)
+    cin >> arr[i];
+
+    int currSum =0;
+    int maxSum = INT_MIN;
+
+    //If Sum < 0, discard the subarray, else store the maximum sum subarray
+    for(int i=0;i<n;i++)
+    {
+        currSum+=arr[i];
+        if (currSum<0)
+        currSum=0;
+
+        maxSum = max(maxSum,currSum);
+    }
+
+    cout << maxSum << endl;
+}
