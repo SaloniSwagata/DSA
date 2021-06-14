499ee79a65c063be63e06fed84d5d97a7c86f577
Saloni <saloniswagata@gmail.com>
2021-06-14 21:20:08 +0530

    Printing the detailed tree

--- Tree/printTree.py
+++ Tree/printTree.py
@@ -15,6 +15,21 @@ def printTree(root):
     printTree(root.left)
     printTree(root.right)
 
+# Detailed printing of tree
+
+def printDetailedTree(root):
+    if root == None:
+        return
+    
+    print(root.data, end=":")
+    if root.left != None:
+        print("L ",root.left.data, end=" ,")
+    if root.right!=None:
+        print("R ",root.right.data)
+    print()
+    printDetailedTree(root.left)
+    printDetailedTree(root.right)
+
 btn1 = BinaryTreeNode(2)
 btn2 = BinaryTreeNode(3)
 btn3 = BinaryTreeNode(4)
