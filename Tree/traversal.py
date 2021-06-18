ceea0e5f947041c2eeb0fb97ac7e40cf7d1c83b0
Saloni <saloniswagata@gmail.com>
2021-06-18 20:07:34 +0530

    Minor changes

--- Tree/traversal.py
+++ Tree/traversal.py
@@ -11,22 +11,22 @@ def InOrder(self,root):
 
 # PreOrder Traversal in a Tree (Root - Left - Right)
 
-def InOrder(self,root):
+def PreOrder(self,root):
         # code here
         if root==None:
             return []
 
-        left_list = self.InOrder(root.left)
-        right_list = self.InOrder(root.right)
+        left_list = self.PreOrder(root.left)
+        right_list = self.PreOrder(root.right)
         return [root.data] + left_list + right_list
 
 # PostOrder Traversal in a Tree (Left - Right - Root)
 
-def InOrder(self,root):
+def PostOrder(self,root):
         # code here
         if root==None:
             return []
 
-        left_list = self.InOrder(root.left)
-        right_list = self.InOrder(root.right)
+        left_list = self.PostOrder(root.left)
+        right_list = self.PostOrder(root.right)
         return left_list + right_list + [root.data]
